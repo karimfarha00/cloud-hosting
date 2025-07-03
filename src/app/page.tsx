b42d771fef413c3aaa5b8b4e36015c452e7d5ed5
Karim Farha <karimfarha00@gmail.com>
2025-07-03 12:36:54 +0300

    Build&Design  HomePage and AboutPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
-
+import Hero from "./components/home/Hero";
+import WebHostingPlan from './components/home/WebHostingPlan';
 
 const HomePage = () => {
-  
   return (
-    <div>
-      
-      <p>Welcome to the Home Page</p>
-     
-      
-    </div>
+    <section>
+      <Hero />
+      <h2 className="text-center mt-10 text-3xl font-bold">
+            Choose Your Web Hosting Plan
+        </h2>
+        <div className='container m-auto flex justify-center items-center my-7 flex-wrap md:gap-7'>
+
+            <WebHostingPlan />
+            <WebHostingPlan />
+            <WebHostingPlan />
+        </div>
+    </section>
   );
 };
 
